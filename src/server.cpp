b4953a533057efa54e5687f076ef0bcdd84915cf
David Yu <lewen.yu@gmail.com>
2018-06-26 09:24:52 -0700

    tidy

--- src/server.cpp
+++ src/server.cpp
@@ -1,68 +1,65 @@
+#include "app_constants.h"
+
 #include<stdio.h>
 #include<string.h>    //strlen
 #include<sys/socket.h>
 #include<arpa/inet.h> //inet_addr
 #include<unistd.h>    //write
- 
+
 int main(int argc , char *argv[])
 {
-    int socket_desc , client_sock , c , read_size;
-    struct sockaddr_in server , client;
     char client_message[2000];
      
     //Create socket
-    socket_desc = socket(AF_INET , SOCK_STREAM , 0);
-    if (socket_desc == -1)
-    {
-        printf("Could not create socket");
+    int socket_desc = socket( AF_INET, SOCK_STREAM, 0 );
+    if ( socket_desc == -1 ) {
+        perror("Could not create socket");
+        return 1;
     }
     puts("Socket created");
-     
+
+    struct sockaddr_in server, client;
     //Prepare the sockaddr_in structure
     server.sin_family = AF_INET;
     server.sin_addr.s_addr = INADDR_ANY;
-    server.sin_port = htons( 8888 );
+    server.sin_port = htons( SERVER_PORT );
      
     //Bind
-    if( bind(socket_desc,(struct sockaddr *)&server , sizeof(server)) < 0)
+    if( bind( socket_desc, (struct sockaddr *)&server , sizeof(server) ) < 0 )
     {
-        //print the error message
-        perror("bind failed. Error");
+        perror( "bind failed. Error" );
         return 1;
     }
-    puts("bind done");
+
+    puts( "bind done" );
      
     //Listen
-    listen(socket_desc , 3);
+    listen( socket_desc, 3 );
      
     //Accept and incoming connection
-    puts("Waiting for incoming connections...");
-    c = sizeof(struct sockaddr_in);
+    puts( "Waiting for incoming connections..." );
+    socklen_t c = sizeof( struct sockaddr_in );
      
     //accept connection from an incoming client
-    client_sock = accept(socket_desc, (struct sockaddr *)&client, (socklen_t*)&c);
-    if (client_sock < 0)
-    {
-        perror("accept failed");
+    int client_sock = accept( socket_desc, (struct sockaddr *)&client, &c );
+    if ( client_sock < 0 ) {
+        perror( "accept failed; could not accept connection from client" );
         return 1;
     }
-    puts("Connection accepted");
+    puts( "Connection accepted" );
      
-    //Receive a message from client
-    while( (read_size = recv(client_sock , client_message , 2000 , 0)) > 0 )
-    {
-        //Send the message back to client
-        write(client_sock , client_message , strlen(client_message));
+    int read_size;
+    // Receive a message from client
+    while ( ( read_size = recv( client_sock, client_message, 2000, 0 ) ) > 0 ) {
+        // Echo message back to client
+        write( client_sock, client_message , strlen( client_message ) );
     }
      
-    if(read_size == 0)
-    {
-        puts("Client disconnected");
+    if ( read_size == 0 ) {
+        puts( "Client disconnected" );
         fflush(stdout);
-    }
-    else if(read_size == -1)
-    {
-        perror("recv failed");
+    } else if ( read_size == -1 ) {
+        perror( "recv failed" );
     }
      
     return 0;
